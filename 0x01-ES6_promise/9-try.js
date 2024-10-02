aecb2d965179e310198c804696744ffd26ccba5d
Bradley Mwangangi <177125727+aceof10@users.noreply.github.com>
2024-10-02 19:09:54 +0300

    style: apply eslint formatting rules

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,13 +1,13 @@
 export default function guardrail(mathFunction) {
-    const queue = [];
+  const queue = [];
 
-    try {
-        const result = mathFunction();
-        queue.push(result);
-    } catch (error) {
-        queue.push(`Error: ${error.message}`);
-    }
+  try {
+    const result = mathFunction();
+    queue.push(result);
+  } catch (error) {
+    queue.push(`Error: ${error.message}`);
+  }
 
-    queue.push("Guardrail was processed");
-    return queue;
+  queue.push('Guardrail was processed');
+  return queue;
 }
